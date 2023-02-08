97d70b6360fb74b31cf528eb32e3ebd156fcb1f0
windliang <648917857@qq.com>
2023-02-08 08:40:05 +0800

    feat: 添加 elementUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui"; // 引入组件
+import "element-ui/lib/theme-chalk/index.css"; // 引入样式
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(ElementUI); // 全局引入
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
