da0189cbbdba7c582474dbaca8277dc85d820785
windliang <648917857@qq.com>
2023-02-22 08:27:04 +0800

    feat: 添加菜单导航

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <router-view />
+    <MenuList class="menu"></MenuList>
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
-
+<script>
+import MenuList from "./components/MenuList/index.vue";
+export default {
+  name: "App",
+  components: {
+    MenuList,
+  },
+};
+</script>
 <style lang="less">
+@menuWidth: 300px;
 #app {
-  background: #f5f5fb;
-  height: 100vh;
-  padding: 0 100px;
+  .menu {
+    position: fixed;
+    width: @menuWidth;
+    top: 0;
+    bottom: 0;
+    left: 0;
+  }
+  .content {
+    margin-left: @menuWidth;
+  }
 }
 body {
   margin: 0;
+  background: #f5f5fb;
 }
 </style>
---
+++ src/components/MenuList/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <el-menu
+      default-active="/"
+      class="el-menu-vertical"
+      @select="handSelect"
+    >
+      <el-menu-item index="/">
+        <i class="el-icon-picture-outline"></i>
+        <span slot="title">画廊</span>
+      </el-menu-item>
+      <el-menu-item index="/tool" >
+        <i class="el-icon-suitcase-1"></i>
+        <span slot="title">工具导航</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuList",
+  props: {},
+  methods: {
+    handSelect(path) {
+        this.$router.push(path)
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.el-menu-vertical {
+    height: 100vh;
+}
+</style>
