77d22f6a5460acf4876d6ad341035f18beb1b89d
windliang <648917857@qq.com>
2023-03-05 18:30:39 +0800

    feat: 引入 leancloud

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import ElementUI from "element-ui"; // 引入组件
 import "element-ui/lib/theme-chalk/index.css"; // 引入样式
-import router from './router'
-
+import router from "./router";
+const AV = require("leancloud-storage");
+AV.init({
+  appId: "7H4TC8BG7PKkt9zb25kKXxQg-gzGzoHsz",
+  appKey: "WUvMpqhSaTyG6DLeCE5vJtI5",
+  serverURL: "https://pc-api.windliang.wang",
+});
+window.$AV = AV;
 Vue.config.productionTip = false;
 Vue.use(ElementUI); // 全局引入
 new Vue({
   router,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
