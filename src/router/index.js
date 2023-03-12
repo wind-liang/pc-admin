a414c8a4d1dccb2bdb76793f5c27ab1f948b0bee
windliang <648917857@qq.com>
2023-03-13 07:46:29 +0800

    feat: 路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -28,4 +28,26 @@ const router = new VueRouter({
   routes,
 });
 
+// 添加路由守卫--导航守卫
+// 作用是通过判断来决定当前的路由跳转到底能不能进行
+// 这种守卫，只要进行路由的跳转就会自动的触发，不能人为调用
+router.beforeEach((to, from, next) => {
+  // to:目标路由
+  // from:源路由
+  // next:下一步的操作，就是用户当前需要进行的操作
+  // 判断当前用户是否登陆，如果登陆了则可以跳转，否则重定向到登陆页
+  // 获取用户登陆之后的token，进行token的判断
+  if (to.path !== "/login") {
+    // 非登录页面验证是否存在token
+    let token = localStorage.getItem("token");
+    if (token) {
+      next();
+    } else {
+      // 说明没有登陆，则重定向到登陆页
+      next({ name: "login" });
+    }
+  } else {
+    next();
+  }
+});
 export default router;
